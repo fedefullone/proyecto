1f3aff1cef34f54c424be2b6257e1b43ed83943f
santi-herrera <santiagotherreras@gmail.com>
2022-11-10 15:26:13 -0300

    inicio post

---
+++ src/screens/NewPost.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import {Text, TextInput, TouchableOpacity,View} from 'react-native';
+import {auth, db} from '../firebase/config';
+
+
+class NewPost extends Component{
+    constructor(){
+        super()
+        this.state={
+            createdAt:'',
+            textoPost:'',
+            photo:''
+        }
+    }
+
+
+
+render(){
+
+
+
+
+}
+}
+    export default NewPost;
